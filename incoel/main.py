664b26c91cdc484cc9af5fc6dab35e218de64b1a
Nikolay <vafedotenko@gmail.com>
2019-08-01 20:22:20 +0300

    changed cmd args and added general metric counting

--- incoel/main.py
+++ incoel/main.py
@@ -33,24 +33,35 @@ def parseXML(xmlFile):
     return boxList
 
 
+tpGlobal = 0
+fpGlobal = 0
+fnGlobal = 0
+tnGlobal = 5
+
 def countMetrics(firstXML, secondXML):
 
-    tp = 0  # element is found in parseXML(firstXML) and is contained in parseXML(secondXML)]
-    fp = 0  # element is found in parseXML(firstXML) but is not contained in parseXML(secondXML)]
-    fn = 0  # element is not found in parseXML(firstXML) but is contained in parseXML(secondXML)]
-    tn = 0  # element is not found in parseXML(firstXML) and is not contained in parseXML(secondXML)] (always true)
+    tp = 0  # element is found in parseXML(firstXML) and is contained in parseXML(secondXML)
+    fp = 0  # element is found in parseXML(firstXML) but is not contained in parseXML(secondXML)
+    fn = 0  # element is not found in parseXML(firstXML) but is contained in parseXML(secondXML)
+    tn = 1  # element is not found in parseXML(firstXML) and is not contained in parseXML(secondXML) (always true)
 
     for i in parseXML(firstXML):
         if i in parseXML(secondXML):
             tp += 1
+            global tpGlobal
+            tpGlobal += 1
 
     for i in parseXML(firstXML):
         if i not in parseXML(secondXML):
             fp += 1
+            global fpGlobal
+            fpGlobal += 1
 
     for i in parseXML(secondXML):
         if i not in parseXML(firstXML):
             fn += 1
+            global fnGlobal
+            fnGlobal += 1
 
     print("=======")
     print("TP: " + str(tp))
@@ -63,5 +74,17 @@ def countMetrics(firstXML, secondXML):
     print("FPR: " + str(fp / (fp + tn)))
 
 
-if __name__ == "__main__":
-    countMetrics("test_task/dst/" + args.d, "test_task/gts/" + args.g)
+if __name__ == "__main__":  # usage: python3 main.py -d test_task/dst/ -g test_task/gts/
+    for it in range(5):
+        countMetrics(args.d + "img_000" + str(it + 1) + "_16-05-2019.xml",
+                     args.g + "img_000" + str(it + 1) + "_16-05-2019.xml")
+    print("\nGeneral metric:")
+    print("=======")
+    print("TP: " + str(tpGlobal))
+    print("TN: " + str(tnGlobal))
+    print("FP: " + str(fpGlobal))
+    print("FN: " + str(fnGlobal))
+    print("=======")
+    print("AC: " + str((tpGlobal + tnGlobal) / (tpGlobal + tnGlobal + fpGlobal + fnGlobal)))
+    print("TPR: " + str(tpGlobal / (tpGlobal + fnGlobal)))
+    print("FPR: " + str(fpGlobal / (fpGlobal + tnGlobal)))
